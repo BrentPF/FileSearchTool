398e15c1a40999aa69a3e4c66695c70a6003b445
BrentPF <Brent.Flores@hotmail.com>
2019-10-03 16:31:40 -0400

    Implemented parsing for .docx and .xlsx files

--- FileSearchTool/Form1.Designer.cs
+++ FileSearchTool/Form1.Designer.cs
@@ -34,31 +34,37 @@
 			this.pathList = new System.Windows.Forms.ListBox();
 			this.folderLabel = new System.Windows.Forms.Label();
 			this.searchButton = new System.Windows.Forms.Button();
+			this.searchProgressBar = new System.Windows.Forms.ProgressBar();
+			this.searchProgressLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// folderBrowserDialogButton
 			// 
-			this.folderBrowserDialogButton.Location = new System.Drawing.Point(12, 38);
+			this.folderBrowserDialogButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.folderBrowserDialogButton.Location = new System.Drawing.Point(12, 41);
 			this.folderBrowserDialogButton.Name = "folderBrowserDialogButton";
 			this.folderBrowserDialogButton.Size = new System.Drawing.Size(95, 22);
 			this.folderBrowserDialogButton.TabIndex = 0;
 			this.folderBrowserDialogButton.Text = "Select Folder";
 			this.folderBrowserDialogButton.UseVisualStyleBackColor = true;
-			this.folderBrowserDialogButton.Click += new System.EventHandler(this.Button1_Click);
+			this.folderBrowserDialogButton.Click += new System.EventHandler(this.folderBrowserDialogButton_Click);
 			// 
 			// searchText
 			// 
+			this.searchText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.searchText.Location = new System.Drawing.Point(113, 12);
 			this.searchText.Name = "searchText";
-			this.searchText.Size = new System.Drawing.Size(532, 20);
+			this.searchText.Size = new System.Drawing.Size(532, 26);
 			this.searchText.TabIndex = 1;
 			// 
 			// pathList
 			// 
+			this.pathList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.pathList.FormattingEnabled = true;
-			this.pathList.Location = new System.Drawing.Point(12, 75);
+			this.pathList.ItemHeight = 20;
+			this.pathList.Location = new System.Drawing.Point(12, 96);
 			this.pathList.Name = "pathList";
-			this.pathList.Size = new System.Drawing.Size(633, 277);
+			this.pathList.Size = new System.Drawing.Size(633, 224);
 			this.pathList.TabIndex = 2;
 			// 
 			// folderLabel
@@ -67,25 +73,46 @@
 			this.folderLabel.BackColor = System.Drawing.Color.White;
 			this.folderLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.folderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.folderLabel.Location = new System.Drawing.Point(113, 38);
+			this.folderLabel.Location = new System.Drawing.Point(113, 41);
 			this.folderLabel.Name = "folderLabel";
 			this.folderLabel.Size = new System.Drawing.Size(532, 22);
 			this.folderLabel.TabIndex = 3;
 			// 
 			// searchButton
 			// 
+			this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.searchButton.Location = new System.Drawing.Point(12, 12);
 			this.searchButton.Name = "searchButton";
-			this.searchButton.Size = new System.Drawing.Size(95, 20);
+			this.searchButton.Size = new System.Drawing.Size(95, 26);
 			this.searchButton.TabIndex = 4;
 			this.searchButton.Text = "Search";
 			this.searchButton.UseVisualStyleBackColor = true;
+			this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+			// 
+			// searchProgressBar
+			// 
+			this.searchProgressBar.Location = new System.Drawing.Point(12, 326);
+			this.searchProgressBar.Name = "searchProgressBar";
+			this.searchProgressBar.Size = new System.Drawing.Size(633, 28);
+			this.searchProgressBar.TabIndex = 5;
+			// 
+			// searchProgressLabel
+			// 
+			this.searchProgressLabel.AutoSize = true;
+			this.searchProgressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.searchProgressLabel.Location = new System.Drawing.Point(12, 77);
+			this.searchProgressLabel.Name = "searchProgressLabel";
+			this.searchProgressLabel.Size = new System.Drawing.Size(325, 16);
+			this.searchProgressLabel.TabIndex = 6;
+			this.searchProgressLabel.Text = "Please enter a search term and select a parent folder.";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(657, 372);
+			this.ClientSize = new System.Drawing.Size(657, 365);
+			this.Controls.Add(this.searchProgressLabel);
+			this.Controls.Add(this.searchProgressBar);
 			this.Controls.Add(this.searchButton);
 			this.Controls.Add(this.folderLabel);
 			this.Controls.Add(this.pathList);
@@ -106,6 +133,8 @@
 		private System.Windows.Forms.ListBox pathList;
 		private System.Windows.Forms.Label folderLabel;
 		private System.Windows.Forms.Button searchButton;
+		private System.Windows.Forms.ProgressBar searchProgressBar;
+		private System.Windows.Forms.Label searchProgressLabel;
 	}
 }
 
--- FileSearchTool/Form1.cs
+++ FileSearchTool/Form1.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace FileSearchTool
 {
@@ -17,7 +21,7 @@ namespace FileSearchTool
 			InitializeComponent();
 		}
 
-		private void Button1_Click(object sender, EventArgs e)
+		private void folderBrowserDialogButton_Click(object sender, EventArgs e)
 		{
 			if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
 			{
@@ -25,8 +29,133 @@ namespace FileSearchTool
 			}
 		}
 
-		private void searchFiles(string expression, string path) {
+		private bool parseFiles(string expression, string path) {
 
+			if (path.Contains(".xlsx"))
+			{
+				Excel.Application xlApp = new Excel.Application();
+				Excel.Workbook xlWorkbook = null;
+				try
+				{
+					xlWorkbook = xlApp.Workbooks.Open(path);
+				}
+				catch
+				{
+					GC.Collect();
+					GC.WaitForPendingFinalizers();
+					xlWorkbook.Close(SaveChanges: false);
+					Marshal.ReleaseComObject(xlWorkbook);
+					xlApp.Quit();
+					Marshal.ReleaseComObject(xlApp);
+					return false;
+				}
+				Excel.Worksheet xlWorksheet = xlWorkbook.Worksheets[1];
+
+				var iTotalColumns = xlWorksheet.UsedRange.Columns.Count;
+				var iTotalRows = xlWorksheet.UsedRange.Rows.Count;
+
+				//These two lines do the magic.
+				xlWorksheet.Columns.ClearFormats();
+				xlWorksheet.Rows.ClearFormats();
+
+				iTotalColumns = xlWorksheet.UsedRange.Columns.Count;
+				iTotalRows = xlWorksheet.UsedRange.Rows.Count;
+				Excel.Range cellRange = xlWorksheet.Range[xlWorksheet.Cells[1, 1], xlWorksheet.Cells[iTotalRows, iTotalColumns]];
+
+				var cellValue = cellRange.Find(
+					What: expression,
+
+					LookIn: Excel.XlFindLookIn.xlValues,
+
+					LookAt: Excel.XlLookAt.xlPart
+
+					);
+
+				GC.Collect();
+				GC.WaitForPendingFinalizers();
+				Marshal.ReleaseComObject(cellRange);
+				Marshal.ReleaseComObject(xlWorksheet);
+				xlWorkbook.Close(SaveChanges: false);
+				Marshal.ReleaseComObject(xlWorkbook);
+				xlApp.Quit();
+				Marshal.ReleaseComObject(xlApp);
+
+				if (cellValue == null)
+				{
+					return false;
+				}
+			}
+			else if (path.Contains(".docx"))
+			{
+				var wdApp = new Word.Application();
+				var wdDoc = wdApp.Documents.Open(path);
+				string body = wdDoc.Content.Text;
+				if (body.Contains(expression))
+				{
+					wdDoc.Close(SaveChanges: false);
+					wdApp.Quit();
+					return true;
+				}
+				
+				wdDoc.Close(SaveChanges: false);
+				wdApp.Quit();
+				return false;
+			}
+			else if (path.Contains(".pdf")) {
+
+			}
+
+			return true;
+		}
+
+		private void SearchButton_Click(object sender, EventArgs e)
+		{
+			if (!String.IsNullOrEmpty(this.searchText.Text) && !String.IsNullOrEmpty(this.folderLabel.Text)) {
+				this.pathList.Items.Clear();
+				this.searchProgressBar.Value = 0;
+				this.pathList.Items.Add("Initiating search...");
+				this.searchButton.Enabled = false;
+				this.folderBrowserDialogButton.Enabled = false;
+				this.searchText.Enabled = false;
+				this.folderLabel.Enabled = false;
+
+				//Allowed extensions
+				string[] extensions = { "*.docx", "*.pdf", "*.xlsx" };
+				string exp = this.searchText.Text;
+				List<string> filePaths = new List<string>();
+
+				//Filepath search
+				foreach (string ext in extensions) {
+					try
+					{
+						filePaths.AddRange(Directory.GetFiles(this.folderLabel.Text, ext, SearchOption.AllDirectories).ToList());
+					}
+					catch (Exception err){
+						this.pathList.Items.Add(err.Message);
+					}
+				}
+				// Set Minimum to 1 to represent the first file being copied.
+				this.searchProgressBar.Minimum = 0;
+				// Set Maximum to the total number of files to copy.
+				this.searchProgressBar.Maximum = filePaths.Count;
+				// Set the Step property to a value of 1 to represent each file being copied.
+				this.searchProgressBar.Step = 1;
+				//Document parse and add to list
+				foreach (string docPath in filePaths) {
+					this.searchProgressLabel.Text = "Processing: " + docPath;
+					if (parseFiles(exp, docPath)) {
+						this.pathList.Items.Add(docPath);
+					}
+					this.searchProgressBar.PerformStep();
+				}
+
+				this.pathList.Items.Add("Search completed.");
+				this.searchProgressLabel.Text = "Document search complete.";
+				this.searchButton.Enabled = true;
+				this.folderBrowserDialogButton.Enabled = true;
+				this.searchText.Enabled = true;
+				this.folderLabel.Enabled = true;
+			}
 		}
 	}
 }
